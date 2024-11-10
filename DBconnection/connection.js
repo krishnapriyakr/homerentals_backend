84805430c1b6aaa1c5d0f34c3a3953e7765c57b4
krishnapriyakr <krishnapriyakrpriya@gmail.com>
2024-11-10 16:45:43 +0530

    homerentals_backend

--- DBconnection/connection.js
+++ DBconnection/connection.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose')
+
+console.log('MongoDB URI:', process.env.DATABASE);
 const connectionString = process.env.DATABASE;
 if (!connectionString) {
     console.error('mongobd connection string is missing');
