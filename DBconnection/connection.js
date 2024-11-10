c109b6f62ac72b7181141795846904370a795312
krishnapriyakr <krishnapriyakrpriya@gmail.com>
2024-11-10 16:31:51 +0530

    homerentals_backend

--- DBconnection/connection.js
+++ DBconnection/connection.js
@@ -1,6 +1,11 @@
-
+require('dotenv').config();
 const mongoose = require('mongoose')
-const connectionString = process.env.DATABASE
+const connectionString = process.env.DATABASE;
+if (!connectionString) {
+    console.error('mongobd connection string is missing');
+    process.exit(1);
+    
+}
 mongoose.connect(connectionString).then(() => {
     console.log("mongoDB connected successfully to hrServer");
 }).catch((err) => {
